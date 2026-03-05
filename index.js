52c7c8b180f61fa9d74296cf86c12d180c955b69
GuilhermeRech08 <guilherme_s_rech@estudante.sesisenai.org.br>
2026-03-05 08:58:39 -0300

    segunda versão

--- index.js
+++ index.js
@@ -1,61 +1,77 @@
 let des = document.getElementById('des').getContext('2d')
 
-let carroInimigo = new CarroInimigo(1300, 325, 50, 50, 'green')
-
-let carroInimigo2 = new CarroInimigo(1500, 200, 50, 50, 'red')
-
-let carro = new Carro(100, 325, 50, 50, 'blue')
+let carroInimigo = new CarroInimigo(1300,325,50,50,'darkgreen')
+let carroInimigo2 = new CarroInimigo(1500,125,50,50,'rgb(247, 0, 255)')
+let carroInimigo3 = new CarroInimigo(1700,400,50,50,'rgb(255, 238, 0)')
+let estrada = new Estrada(10,345,40,10,'white')
+let estrada2 = new Estrada(80,345,40,10,'white')
+let estrada3 = new Estrada(140,345,40,10,'white')
+let carro = new Carro(100,325,50,50,'darkblue')
+let medidaCarro = new Carro(100, 325, 85, 60, 'green')
 
 document.addEventListener('keydown', (e)=>{
-    if(e.key === 's'){
-        carro.dir = +5
-    console.log(carro.dir)
-    }else if(e.key === 'w'){
-        carro.dir = -5
-    console.log(carro.dir)
-    }
-    
+    if(e.key === 'w' || e.key === 'ArrowUp'){
+        carro.dir -= 10
+    }else if(e.key === 's' || e.key === 'ArrowDown'){
+        carro.dir += 10
+    }    
 })
 
 document.addEventListener('keyup', (e)=>{
-    if(e.key === 'w' || e.key === 's'){
-        carro.dir = 0   
+    if(e.key === 'w' || e.key === 'ArrowUp'){
+        carro.dir = 0
+    }else if(e.key === 's' || e.key === 'ArrowDown'){
+        carro.dir = 0
     }
 })
 
-
-document.addEventListener('keydown', (e)=>{
-    if(e.key === 'a'){
-        carro.dir2 = -5
-    console.log(carro.dir)
-    }else if(e.key === 'd'){
-        carro.dir2 = 5
-    console.log(carro.dir)
+function colisao(){
+    if(carro.colid(carroInimigo)){
+        carroInimigo.recomeca()
+        carro.vida -= 1
+        
     }
+    if(carro.colid(carroInimigo2)){
+        carroInimigo2.recomeca()
+        carro.vida -= 1
+    }
+    if(carro.colid(carroInimigo3)){
+        carroInimigo3.recomeca()
+        carro.vida -= 1
+    }
+    console.log(carro.vida)
+}
+
+function pontuacao(){
     
-})
-
-document.addEventListener('keyup', (e)=>{
-    if(e.key === 'a' || e.key === 'd'){
-        carro.dir2 = 0   
-    }
-})
-
+}
 
 function desenha(){
-    carroInimigo.des_quad()
-    carroInimigo2.des_quad()
-    carro.des_quad()
+    // estrada.des_quad()
+    // estrada2.des_quad()
+    // estrada3.des_quad()
+    carroInimigo.des_carro()
+    carroInimigo2.des_carro()
+    carroInimigo3.des_carro()
+    carro.des_carro()
+    medidaCarro.des_quad()    
 }
 
 function atualiza(){
     carro.mov_car()
-    carroInimigo.mov_car()    
+    carroInimigo.mov_car()
     carroInimigo2.mov_car()
+    carroInimigo3.mov_car()
+    // estrada.mov_est()
+    // estrada2.mov_est()
+    // estrada3.mov_est()
+    colisao()
+    pontuacao()
+
 }
 
 function main(){
-    des.clearRect(0, 0, 1200, 700)
+    des.clearRect(0,0,1200,700)
     desenha()
     atualiza()
     requestAnimationFrame(main)
